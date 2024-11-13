34f25dfc032e2b3c36907512b0f8ad5fc634d1a4
milicag93 <mickomali93@gmail.com>
2024-11-12 20:41:17 -0500

    secondcommit

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -2,6 +2,10 @@ public class test {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("This is the project version 1");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
     }
 }
-//done with version one
+//done with version two
