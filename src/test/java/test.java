148f5eb447caf84b973e9259978b714583f34340
milicag93 <mickomali93@gmail.com>
2024-11-12 20:48:02 -0500

    third commit

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,11 +1,8 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println("This is the project version 1");
-        System.out.println("Hello world");
-        System.out.println("Hello world");
-        System.out.println("Hello world");
-        System.out.println("Hello world");
+        System.out.println("This is the project version 3");
+
     }
 }
-//done with version two
+//done with version three
