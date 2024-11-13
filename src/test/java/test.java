db877ae7b498b58eef3a61af356aac69659d054d
milicag93 <mickomali93@gmail.com>
2024-11-12 20:12:21 -0500

    1stname

---
+++ src/test/java/test.java
@@ -0,0 +1,7 @@
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("This is the project version 1");
+    }
+}
+//done with version one
